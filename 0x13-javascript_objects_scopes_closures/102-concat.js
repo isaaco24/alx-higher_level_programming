7460cb13b4fd0e0083bb899e8e2dd5bc44e45769
isaaco24 <isaacadegboyega24@gmail.com>
2023-11-14 08:22:42 -0800

    0x13java

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
+const fArg = fs.readFileSync(process.argv[2], 'utf8'); // Specify 'utf8' encoding
+const sArg = fs.readFileSync(process.argv[3], 'utf8'); // Specify 'utf8' encoding
 fs.writeFileSync(process.argv[4], fArg + sArg);
